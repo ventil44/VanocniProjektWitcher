dc1ed8e6ae908fa57b1995ac4350a5400dccaaad
ventil44 <vesdenda@gmail.com>
2026-02-05 21:02:59 +0100

    pridana trida Dialog

---
+++ src/Dialogs/Dialog.java
@@ -0,0 +1,31 @@
+package Dialogs;
+
+import java.util.ArrayList;
+
+public class Dialog {
+
+    private String id;
+    private String npcId;
+    private String locationId;
+
+    private String lines;
+
+    private String linesBefore;
+    private String question;
+    private String correctAnswer;
+    private String successLines;
+    private String failLines;
+
+    public String getId() { return id; }
+    public String getNpcId() { return npcId; }
+    public String getLocationId() { return locationId; }
+
+    public String getLines() { return lines; }
+
+    public String getLinesBefore() { return linesBefore; }
+    public String getQuestion() { return question; }
+    public String getCorrectAnswer() { return correctAnswer; }
+    public String getSuccessLines() { return successLines; }
+    public String getFailLines() { return failLines; }
+
+}
