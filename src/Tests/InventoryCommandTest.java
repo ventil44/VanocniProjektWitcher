d7ab92e7d5d6a90b7cc37e5ca9e41649c28409fd
ventil44 <vesdenda@gmail.com>
2026-02-12 14:00:04 +0100

    TalkCommandTest a InventoryCommandTest done

---
+++ src/Tests/InventoryCommandTest.java
@@ -0,0 +1,32 @@
+package Tests;
+
+import Command.InventoryCommand;
+import Items.Potion;
+import Player.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryCommandTest {
+
+    Player player;
+    InventoryCommand command;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("Zbinek", 100);
+        command = new InventoryCommand(player);
+    }
+
+    @Test
+    void execute() {
+
+        Potion potion = new Potion("Potion", 30);
+        player.getInventory().add(potion);
+
+        String result = command.execute();
+
+        assertEquals("Inventory:1 - Potion\n", result);
+    }
+}
---
+++ src/Tests/TalkCommandTest.java
@@ -0,0 +1,66 @@
+package Tests;
+
+import Command.TalkCommand;
+import Dialogs.Dialog;
+import Game.GameData;
+import Game.GameWorld;
+import Locations.Location;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TalkCommandTest {
+
+    Location location;
+    TalkCommand command;
+    FakeWorld fakeWorld;
+
+    private static class FakeWorld extends GameWorld {
+
+        private static final HashMap<String, Location> map = new HashMap<>();
+        private final ArrayList<Dialog> dialogs = new ArrayList<>();
+
+        public FakeWorld(GameData data, String startLocationId) {
+            super(data, startLocationId);
+        }
+
+        static void add(Location loc) {
+            map.put(loc.getId(), loc);
+        }
+
+        @Override
+        public Location findLocation(String id) {
+            return map.get(id);
+        }
+
+        @Override
+        public ArrayList<Dialog> getDialogs() {
+            return dialogs;
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        location = new Location("a", "Start", "Start location", new ArrayList<>());
+
+        FakeWorld.add(location);
+
+        GameData data = new GameData();
+        fakeWorld = new FakeWorld(data, "a");
+        fakeWorld.setCurrentLocation(location);
+
+        Scanner scanner = new Scanner("");
+        command = new TalkCommand(fakeWorld, scanner);
+    }
+
+    @Test
+    void execute() {
+        String result = command.execute();
+        assertEquals("Není tu nikdo, s kým by se dalo mluvit.", result);
+    }
+}
