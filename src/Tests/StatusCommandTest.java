a5a00448849fdecee14ab868c0a9eb98e1512c45
ventil44 <vesdenda@gmail.com>
2026-02-12 20:32:14 +0100

    StatusCommandTest javadocs

--- src/Tests/StatusCommandTest.java
+++ src/Tests/StatusCommandTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Tests the StatusCommand
+ * @author Denis Vesely
+ */
 class StatusCommandTest {
 
     Player player;
