28d4df7d99fa3a2b4c8ffdc12fae3e7e6199a423
ventil44 <vesdenda@gmail.com>
2026-02-12 19:33:19 +0100

    AttackCommand javadocs

--- src/Characters/NPC.java
+++ src/Characters/NPC.java
@@ -12,10 +12,6 @@ public class NPC {
 
         public NPC(String name) {}
 
-        public String talk() {
-            return "NPC " + name + ": " + dialog;
-        }
-
 
     }
 
--- src/Command/AttackCommand.java
+++ src/Command/AttackCommand.java
@@ -5,6 +5,10 @@ import Game.GameWorld;
 import Locations.Location;
 import Player.Player;
 
+/**
+ * Command that handles combat between the player and enemies
+ * @author Denis Vesely
+ */
 public class AttackCommand implements Command {
 
     private final Player player;
@@ -16,6 +20,12 @@ public class AttackCommand implements Command {
         this.gameWorld = gameWorld;
     }
 
+    /**
+     * executes turned based combat in current location
+     * if theres no enemy in current location, the command returns a message telling the user theres no one to fight.
+     * When the fight is over, the command returns the whole combat log. If player dies the command tells the user hes dead.
+     * @return a message with the outcome of the fight or a message that theres no one to fight
+     */
     @Override
     public String execute() {
         Location loc = gameWorld.getCurrentLocation();
