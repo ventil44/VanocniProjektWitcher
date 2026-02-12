5732f927df60f7337c2cf187258e6b7b2fe97740
ventil44 <vesdenda@gmail.com>
2026-02-12 19:51:13 +0100

    StatusCommand javadocs

--- src/Command/StatusCommand.java
+++ src/Command/StatusCommand.java
@@ -2,14 +2,23 @@ package Command;
 
 import Player.Player;
 
+/**
+ * Command that displays the players HP
+ * @author Denis Vesely
+ */
 public class StatusCommand implements Command {
 
     private final Player player;
+
 
     public StatusCommand(Player player) {
         this.player = player;
     }
 
+    /**
+     * Executes status command
+     * @return players current HP
+     */
     @Override
     public String execute() {
         return "HP: " + player.getHealth();
